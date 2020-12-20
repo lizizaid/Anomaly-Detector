04b8f8a84ed893c46b5ba4150f07c8781f746261
Lee <lizizaid@gmail.com>
2020-12-20 18:21:00 +0200

    After targil 2

--- minCircle.h
+++ minCircle.h
@@ -24,15 +24,6 @@ public:
 };
 // --------------------------------------
 
-vector<Point> putInVector(Point** points, int size){
-    vector<Point> v = {};
-    for (int i = 0; i < size; i++){
-        Point p = *points[i];
-        v.push_back(p);
-    }
-    return v;
-}
-
 float distance(Point p1, Point p2){
     return sqrt(pow((p1.x-p2.x),2) + pow((p1.y-p2.y),2));
 }
